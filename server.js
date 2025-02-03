6e979f9e34d6ae00873138e5142f5bf13de01b28
fatemaalajaimi <fatemaalajaimi@gmail.com>
2025-02-03 19:13:57 +0300

    initialize server.js and DB connection

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+// const authRouter = require('./controllers/auth')
+// const userRouter = require('./controllers/user')
+// const { verifyToken } = require('./middleware/jwtUtils')
+dotenv.config()
+
+const app = express()
+app.use(express.static('public'))
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(express.json())
+
+// Routes
+// app.use('/auth', authRouter)
+// app.use('/user', verifyToken, userRouter)
+// app.use('/pharmacy', pharmacyRoutes)
+// app.use('/item', itemRoutes)
+
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+
+// Start Server
+app.listen(4000, () => console.log('Server running...'))
